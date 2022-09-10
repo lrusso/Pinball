db269d1f0437c9b3d72cb8a395a69bbce3c77b78
Leonardo Russo <info@lrusso.com>
2022-09-09 23:23:09 -0300

    Add files via upload

--- PinballGame.js
+++ PinballGame.js
@@ -345,7 +345,7 @@ Pinball.Menu.prototype = {
 				{
 				var date = new Date();
 				date.setTime(date.getTime() + (days*24*60*60*1000));
-				expires = "; expires=" + date.toUTCString();
+				expires = "; expires=" + date.toUTCString() + "; SameSite=Lax";
 				}
 			document.cookie = name + "=" + (value || "")  + expires + "; Secure; path=/";
 			}
